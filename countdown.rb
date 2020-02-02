635713f34c163ca421c50d321307d1d6eef5539c
Bryan L Bottorff <bryanlbottorff@gmail.com>
2020-02-02 19:07:00 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,8 +8,7 @@ end
 return "HAPPY NEW YEAR!"
 end
 
-def countdown_with_sleep
-  sleep 1.second until number == 0
+def countdown_with_sleep(1)
     while number > 0
       puts "#{number} SECOND(S)!"
       number -= 1
